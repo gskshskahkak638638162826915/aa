702416315c4f406a1966b29a9657f306c443ce4d
gskshskahkak638638162826915 <145205795+gskshskahkak638638162826915@users.noreply.github.com>
2023-09-16 19:04:00 +0600

    Create a.js

---
+++ a.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    const folderName = Math.random().toString(36).substring(7);
+  
+    const folderPath = path.join(__dirname, 'uploads', folderName);
+    fs.mkdirSync(folderPath);
+    callback(null, folderPath);
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post('/', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  const filePath = path.join(req.file.destination, req.file.filename);
+  res.status(200).send(filePath);
+});
+
+app.listen(6381, () => {
+  console.log(`Server is running on port ${port}`);
+});
